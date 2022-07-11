1f78f7a2e5d4d1d883c7ca315e9a2c795dc99505
rohkulkagit <ms.rohini.kulkarni@gmail.com>
2022-07-11 10:19:12 +0530

    creating conflict

--- myprogram.java
+++ myprogram.java
@@ -1,5 +1,5 @@
 this is my java file
 This modification done i intellijidea - line modified appended by person2
-line modified by me in intellij - creating conflict
+line modified by me in intellij - creating conflict - added in intellij
 This is mofied in github say by person2
 fouth line modified in git by person2
